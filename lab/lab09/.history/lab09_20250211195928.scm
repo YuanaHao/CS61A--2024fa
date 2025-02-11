4b3a515361f2d1a97fe1c48b12bc685626133d85
YuanaHao <youthandqueen@qq.com>
2025-02-11 20:30:02 +0800

    finished lab09

---
+++ lab/lab09/.history/lab09_20250211195928.scm
@@ -0,0 +1,28 @@
+(define (over-or-under num1 num2)
+  (cond 
+    ((> num1 num2) 1)
+    ((< num1 num2) -1)
+    (else 0))
+)
+
+(define (make-adder num) 
+  (lambda (x) (+ x num))
+)
+
+(define (composed f g)
+  (lambda (x) (f (g x)))
+)
+
+(define (repeat f n) 'YOUR-CODE-HERE)
+
+(define (max a b)
+  (if (> a b)
+      a
+      b))
+
+(define (min a b)
+  (if (> a b)
+      b
+      a))
+
+(define (gcd a b) 'YOUR-CODE-HERE)
---
+++ lab/lab09/.history/lab09_20250211202748.scm
@@ -0,0 +1,39 @@
+(define (over-or-under num1 num2)
+  (cond 
+    ((> num1 num2) 1)
+    ((< num1 num2) -1)
+    (else 0))
+)
+
+(define (make-adder num) 
+  (lambda (x) (+ x num))
+)
+
+(define (composed f g)
+  (lambda (x) (f (g x)))
+)
+
+(define (repeat f n)
+  (if (= n 0)
+      (lambda (x) x)
+      (composed f (repeat f (- n 1)))
+  )
+)
+
+(define (max a b)
+  (if (> a b)
+      a
+      b))
+
+(define (min a b)
+  (if (> a b)
+      b
+      a))
+
+(define (gcd a b)
+  (cond 
+    ((= b 0) a)
+    ((> a b) (gcd b (modulo a b)))
+    (else (gcd b a))
+  )
+)
