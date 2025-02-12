a28005350150e9227dc9668e031e391d4b89e6fc
YuanaHao <youthandqueen@qq.com>
2025-02-12 10:45:07 +0800

    finished hw08

---
+++ hw/hw08/.history/hw08_20250212103648.scm
@@ -0,0 +1,24 @@
+(define (ascending? s)
+  (cond ((null? s) #t)
+        ((null? (cdr s)) #t)
+        (else (and (<= (car s) (cadr s))
+                  (ascending? (cdr s))))))
+
+(define (my-filter pred s)
+  (cond ((null? s) nil)
+        ((pred (car s)) (cons (car s) (my-filter pred (cdr s))))
+        (else (my-filter pred (cdr s)))))
+
+(define (interleave lst1 lst2)
+  (cond ((null? lst1) lst2)
+        ((null? lst2) lst1)
+        (else (cons (car lst1)
+                   (cons (car lst2)
+                         (interleave (cdr lst1) (cdr lst2)))))))
+
+(define (no-repeats s)
+  (if (null? s)
+      nil
+      (cons (car s)
+            (no-repeats (my-filter (lambda (x) (not (= x (car s))))
+                                 (cdr s))))))
